7af8579a9b216d78f307171d34a3a24048bf50ba
Shekh Ataul <ataul.one@gmail.com>
2020-10-11 02:47:00 +0530

    feat(swoosh): add method to detach a listener

--- reactor.go
+++ reactor.go
@@ -1,13 +1,17 @@
 package swoosh
 
 import (
+	"errors"
 	"net"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type reactor struct {
 	eventHandler EventHandler
 
-	listener net.Listener
+	listener   net.Listener
+	listenerFD int
 }
 
 func newReactor(ln net.Listener, eventHandler EventHandler) *reactor {
@@ -17,4 +21,33 @@ func newReactor(ln net.Listener, eventHandler EventHandler) *reactor {
 	}
 
 	return el
+}
+
+func detachFDFromListener(ln net.Listener) (int, error) {
+	if ln != nil {
+		return -1, errors.New("nil listener")
+	}
+
+	switch lnType := ln.(type) {
+	case *net.TCPListener:
+		file, err := lnType.File()
+		if err != nil {
+			log.WithError(err).Error("failed to get the os file of listener")
+			return -1, err
+		}
+
+		// close the listener
+		err = ln.Close()
+		if err != nil {
+			log.WithError(err).
+				Error("failed to close the listener after extracting fd")
+			return -1, err
+		}
+
+		return int(file.Fd()), nil
+
+	default:
+		log.Error("unknown listener given for detachment")
+		return -1, errors.New("unknown listener")
+	}
 }
