8f5739037f40424fdf7c54c72e92780336ffd322
Shekh Ataul <ataul.one@gmail.com>
2020-10-10 21:00:08 +0530

    feat(swoosh): add client connection interface

---
+++ connection.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 Shekh Ataul
+// This code is licensed under MIT license (see LICENSE for details)
+
+package swoosh
+
+// Conn represents a thread safe client connection.
+type Conn interface{}
