bb3eefaaa49a9f022da26fce44ef27074337b0b2
Shekh Ataul <ataul.one@gmail.com>
2020-10-11 17:02:30 +0530

    feat(swoosh): add Conn implementation

--- connection.go
+++ connection.go
@@ -3,5 +3,34 @@
 
 package swoosh
 
+import (
+	"bytes"
+	"net"
+)
+
 // Conn represents a thread safe client connection.
 type Conn interface{}
+
+type conn struct {
+	fd int
+
+	readStream  *bytes.Buffer
+	writeStream *bytes.Buffer
+
+	localAddr  net.Addr
+	remoteAddr net.Addr
+}
+
+func newTCPConn(connFD int, remoteAddr, localAddr net.Addr) *conn {
+	c := &conn{
+		fd:         connFD,
+		localAddr:  localAddr,
+		remoteAddr: remoteAddr,
+
+		// We can use a pool of buffers here
+		readStream:  bytes.NewBuffer(make([]byte, 4096)),
+		writeStream: bytes.NewBuffer(make([]byte, 4096)),
+	}
+
+	return c
+}
