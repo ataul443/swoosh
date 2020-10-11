40471b5a0730b8b0a83e7266436702ce1402764f
Shekh Ataul <ataul.one@gmail.com>
2020-10-11 13:54:08 +0530

    feat(in/snet): add accept variant for unix distros

---
+++ internal/snet/accept_unix.go
@@ -0,0 +1,20 @@
+// +build unix
+
+package snet
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// Accept accepts a new connection from provided lsitener file descriptor
+func Accept(listenerFD int) (connFD int, sockAddr unix.Sockaddr, err error) {
+	connFD, sockAddr, err = unix.Accept(listenerFD)
+	if err != nil {
+		err = os.NewSyscallError("accept", err)
+		return -1, nil, err
+	}
+
+	return connFD, sockAddr, err
+}
