9f04f02b457b651a8e89a6efae15e50d1d8b6b56
Shekh Ataul <ataul.one@gmail.com>
2020-10-11 17:42:16 +0530

    feat(in/poll): add method to put fds in epoll with read events on

--- internal/poll/epoll_linux.go
+++ internal/poll/epoll_linux.go
@@ -16,10 +16,13 @@ const (
 		unix.EPOLLERR
 
 	// ReadEvents represents read epoll events.
-	ReadEvents = unix.EPOLLIN | ExceptionEvents
+	ReadEvents = unix.EPOLLIN | unix.EPOLLPRI
 
 	// WriteEvents represents write epoll events.
-	WriteEvents = unix.EPOLLOUT | ExceptionEvents
+	WriteEvents = unix.EPOLLOUT
+
+	// ReadWriteEvents represents both read and write epoll events.
+	ReadWriteEvents = ReadEvents | WriteEvents
 )
 
 // Poller is responsible for watching file descriptors for events.
@@ -46,4 +49,23 @@ func New(logger *log.Logger) (*Poller, error) {
 	}
 
 	return p, nil
+}
+
+// AddRead put the fs in epoll's interest list with read event on.
+func (p *Poller) AddRead(fd int) error {
+	err := unix.EpollCtl(p.epfd, unix.EPOLL_CTL_ADD,
+		fd, &unix.EpollEvent{
+			Events: ReadEvents,
+			Fd:     int32(fd),
+		})
+
+	if err != nil {
+		err = os.NewSyscallError("epoll_ctl", err)
+		p.logger.WithError(err).WithField("fd", fd).
+			Warn("failed to add in epoll's interest list with read event")
+	}
+
+	p.logger.WithField("fd", fd).
+		Trace("added to epoll's interest list with read event")
+	return err
 }
