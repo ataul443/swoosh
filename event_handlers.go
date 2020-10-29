da9a992bb00959e273966b2404b9c50371b24248
Shekh Ataul <ataul.one@gmail.com>
2020-10-30 04:17:53 +0530

    change(swoosh): event handlers signature returns Action

--- event_handlers.go
+++ event_handlers.go
@@ -1,15 +1,24 @@
 package swoosh
 
+// Action is a reaction taken by the server after an event.
+type Action uint
+
+const (
+	CloseConnection Action = iota
+
+	None
+)
+
 // EventHandler represents handlers which are going to be provided
 // to a swoosh listener.
 type EventHandler interface {
 
 	// OnConnOpen will fire just after a new connection opens.
-	OnConnOpen(conn Conn) error
+	OnConnOpen(conn Conn) (out []byte, action Action)
 
 	// OnConnPacket will fire when a connection has data from client.
-	OnConnPacket(conn Conn) error
+	OnConnPacket(conn Conn) (out []byte, action Action)
 
 	// OnConnClose will fire just after a connection closes.
-	OnConnClose(conn Conn) error
+	OnConnClose(conn Conn) (action Action)
 }
